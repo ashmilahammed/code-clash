9f3e56b67dbc81c803804ba7c0385202cb8acf81
Ashmil Ahammed <ashmilahammed0@gmail.com>
2026-04-17 11:42:23 +0530

    fix : fix the image send issue by increasing storage capacity

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -36,7 +36,9 @@ app.use(cors({
   ],
   credentials: true
 }));
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use(cookieParser());
 
 // Routes
